d157fb05a5854a885e0091b913da94949a199d54
debabrataChakraborty <debabrata15-674@diu.edu.bd>
2025-01-12 14:23:53 +0600

    news container done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,9 +4,13 @@ import Navbar from '../../Shared/Navbar/Navbar';
 import LeftSideNav from '../../Shared/LeftSideNav/LeftSideNav';
 import RightSideNav from '../../Shared/RightSideNav/RightSideNav';
 import BreakingNews from '../BreakingNews/BreakingNews';
+import NewsCard from '../NewsCard/NewsCard';
+import { useLoaderData } from "react-router-dom";
 
 
 const Home = () => {
+    const news = useLoaderData();
+    console.log(news)
     return (
         <div>
             <Header></Header>
@@ -15,16 +19,21 @@ const Home = () => {
             <h1 className='text-3xl font-poppins'>this is home</h1>
 
            <div className='grid grid-cols-1 lg:grid-cols-4 gap-6'>
-           <div className='border border-blue-600'>
+           <div>
                 <LeftSideNav></LeftSideNav>
             </div>
 
 
-            <div className='md:col-span-2 border border-blue-800'>
-                <h2 className='text-4xl'>news coming soon</h2>
+            <div className='md:col-span-2'>
+              {
+                news.map(aNews => <NewsCard
+                key={aNews._id}
+                news ={aNews}
+                    ></NewsCard> )
+              }
             </div>
 
-            <div className='border border-blue-800'>
+            <div>
                 <RightSideNav></RightSideNav>
             </div>
            </div>
